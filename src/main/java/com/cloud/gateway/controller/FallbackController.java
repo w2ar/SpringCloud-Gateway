6727bce01310e8c6245a24f94a1cab431b0a70a3
Arvin WW <ww.arvin@gmail.com>
2019-03-29 14:05:08 +0800

    no message

--- src/main/java/com/cloud/gateway/controller/FallbackController.java
+++ src/main/java/com/cloud/gateway/controller/FallbackController.java
@@ -3,11 +3,16 @@ package com.cloud.gateway.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSONObject;
+
 @RestController
 public class FallbackController {
 
-	@RequestMapping("/fallback")
+	@RequestMapping("/clientFallback")
 	public String fallback() {
-		return "error";
+		JSONObject r = new JSONObject();
+		r.put("state", "9999");
+		r.put("msg", "Cleint --> 系统错误，请求失败");
+		return r.toJSONString();
 	}
 }
